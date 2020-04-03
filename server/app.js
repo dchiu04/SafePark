5bc4026681af419b319405ee25223fca416eb05e
JasSC97 <Montreal231>
2020-04-03 03:33:01 -0700

    created the get request responsible for generating the pousson probability of being robbed in a given area in the next hour, need to find a host and host it as well as clean up the code to make it more readable and documented.

--- server/app.js
+++ server/app.js
@@ -3,29 +3,52 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const url = require('url');
 const GeoPoint = require('geopoint')
+ var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "vancouver-vehicular-crimes.c4ztbtstxmqe.us-east-1.rds.amazonaws.com",
+  user: "admin",
+  password: "debbysucks"
+});
+
 
 const app = express();
 
+
+// three parameters for this request:
+// lon : the longitude as a double
+// lat : the latitude as a double
+// distance : the distance, in miles, as a double
 app.get('/', (req, res) => {
     console.log(req.params)
     var lat = req.query.lat
     var lon = req.query.lon
-    console.log(`lat: ${req.query.lat}`)
-    console.log(`lon: ${req.query.lon}`)
-    current_point = new GeoPoint(parseFloat(req.query.lat), parseFloat(req.query.lon))
-    console.log(current_point)
-    coords = current_point.boundingCoordinates(5)
-    var min_lat = coords[0].latitude()
-    var min_lon = coords[0].longitude()
-    var max_lat = coords[1].latitude()
-    var max_lon = coords[1].longitude()
-    console.log(min_lat)
-    console.log(min_lon)
-    console.log(max_lat)
-    console.log(max_lon)
+    var distance = req.query.distance
 
-    
+    con.query(`SELECT *, (
+                3959 * acos (
+                cos ( radians(${lat}) )
+                * cos( radians( lat ) )
+                * cos( radians( lng ) - radians(${lon}) )
+                + sin ( radians(${lat}) )
+                * sin( radians( lat ) )
+                )
+                ) AS distance
+                FROM crimes.vehicular
+                HAVING distance < ${distance}
+                ORDER BY distance`, 
+            (err, result, fields) => {
+                if (err) throw err;
+                
+                var crimesPerHour = (result.length / 140160)
 
+                response = {'count': result.length,
+                            'crimesPerHour': crimesPerHour,
+                            'probabilityOfACrimeInTheNextHour': crimesPerHour * Math.exp(-1.0 * crimesPerHour),
+                            'queries': result}
+                res.send(response)
+                console.log(response);
+            });
 });
 
 const PORT = process.env.PORT || 8000;
